0665ee55662e1fb195817679da6deff316865c03
GHNYW <guilhermegm6459740@gmail.com>
2025-07-28 12:48:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Você pode adicionar interações JS aqui
+document.querySelector('form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Mensagem enviada! Em breve entraremos em contato.');
+});
